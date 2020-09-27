e502f7dd0c64fab8ad9f6b59c6a5dca1ba6de47c
JannisKohle <Jannis.Kohle@icloud.com>
2020-09-27 11:45:44 +0200

    changed parameters to options

--- implementation.c
+++ implementation.c
@@ -7,7 +7,7 @@ const usage = "\nUSAGE:\n\nnoterman add\nnoterman change [id]\nnotermna delete [
 
 int main(int argc, char *argv[]) {
     if(argc==2) {
-        if(argv[1]=="add") {}
+        if(argv[1]=="--add") {}
         else {
             printf(usage);
             return 1;
@@ -15,8 +15,8 @@ int main(int argc, char *argv[]) {
     }
 
     else if(argc==3) {
-        if(argv[1]=="change") {}
-        else if(argv[1]=="delete") {}
+        if(argv[1]=="--change") {}
+        else if(argv[1]=="--delete") {}
         else {
             printf(usage);
             return 1;
