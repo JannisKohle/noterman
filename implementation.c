25f4e928ac2c80403b03fd89cf84cdf0c5f46ad9
JannisKohle <Jannis.Kohle@icloud.com>
2020-09-27 11:34:20 +0200

    started c implementation

---
+++ implementation.c
@@ -0,0 +1,30 @@
+// noterman implementation in C
+
+#include <stdlib.h>
+#include <stdio.h>
+
+const usage = "\nUSAGE:\n\nnoterman add\nnoterman change [id]\nnotermna delete [id]\n\n";
+
+int main(int argc, char *argv[]) {
+    if(argc==2) {
+        if(argv[1]=="add") {}
+        else {
+            printf(usage);
+            return 1;
+        }
+    }
+
+    else if(argc==3) {
+        if(argv[1]=="change") {}
+        else if(argv[1]=="delete") {}
+        else {
+            printf(usage);
+            return 1;
+        }
+    }
+
+    else {
+        printf(usage);
+        return 1;
+    }
+}
